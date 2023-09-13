adef5b5e5777042ccdb438805df9ee627722b305
Chris Wayman <wayman0@pnw.edu>
2023-09-13 18:08:52 -0500

    Worked on Translate grs/obj.  grs got restructured

--- assets/TranslateOBJ.js
+++ assets/TranslateOBJ.js
@@ -1,6 +1,88 @@
 //@ts-check
+import {createReadStream, createWriteStream} from "node:fs";
+import * as ReadLine from "node:readline";
 
-import { execFile } from "node:child_process";
+if(process.argv.length != 6)
+   throw new Error("Usage: node TranslateOBJ <OBJ-File-Name> [<x-amount> [<y-amount> [<z-amount>]]]");
+
+const file = process.argv[2];
+const xTrans = +process.argv[3];
+const yTrans = +process.argv[4];
+const zTrans = +process.argv[5];
+
+const inputStream = createReadStream(file);
+const readLine = ReadLine.createInterface(
+                           { input: inputStream, 
+                             crlfDelay: Infinity
+                           });
+
+const outputPath = file.substring(1 + file.indexOf("\\"), file.indexOf(".")) 
+                                    + "-Trans(" + xTrans + ", " + yTrans +  ", " + zTrans + ").obj";
+const outputStream = createWriteStream(outputPath, { encoding: "utf8" });
+
+for await (const line of readLine)
+{
+   if(   line.startsWith("#")
+      || line.startsWith("vt")
+      || line.startsWith("vn")
+      || line.startsWith("f")
+      || line.startsWith("s")
+      || line.startsWith("g")
+      || line.startsWith("o")
+      || line.startsWith("usemtl")
+      || line.startsWith("mtllib"))
+   {
+      outputStream.write(line + "\n");
+   }
+   else if(line.startsWith("v"))
+   {
+      const vertexLine = line.substring(line.indexOf("v") + 2);
+      const nums = vertexLine.split("  ");
+
+      const x = +nums[0];
+      const y = +nums[1];
+      const z = +nums[2];
+
+      outputStream.write("v  ");
+      outputStream.write((x + xTrans).toFixed(6) + "  " +
+                         (y + yTrans).toFixed(6) + "  " + 
+                         (z + zTrans).toFixed(6) + "  " + "\n");
+   }
+   else
+   {
+      outputStream.write(line + "\n");
+      console.log(line);
+   }
+}
+
+inputStream.close();
+outputStream.close();
+
+
+
+/*
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(`Line from file: ${line}`);
+  }
+}
+*/
+
+
+/*
 import {createWriteStream, readFileSync} from "node:fs";
 
 if(process.argv.length != 6)
@@ -59,3 +141,4 @@ while(lineIndex < fileLines.length)
 
    outputStream.close();
 }
+*/
--- assets/grs/TranslateGRS.js
+++ assets/grs/TranslateGRS.js
@@ -1,5 +1,424 @@
 //@ts-check
 
+import {createReadStream, createWriteStream} from "node:fs";
+import * as ReadLine from "node:readline";
+
+if(process.argv.length != 5)
+   throw new Error("Usage: node TranslateGRS.js <GRS-File-Name> [<x-amount> [<y-amount>]]");
+
+const file = process.argv[2];
+const xTrans = +process.argv[3];
+const yTrans = +process.argv[4];
+
+const inputStream = createReadStream(file);
+const readLine = ReadLine.createInterface(
+                           { input: inputStream, 
+                             crlfDelay: Infinity
+                           });
+
+const outputPath = file.substring(1 + file.indexOf("\\"), file.indexOf(".")) 
+                                    + "-Trans(" + xTrans + ", " + yTrans + ").grs";
+const outputStream = createWriteStream(outputPath, { encoding: "utf8" });
+
+// create a variable to keep track of if we have hit the star line
+let hitStars = false;
+
+// count the lines we have read 
+let linesCounted = 0;
+
+// number of chunks of vertices
+let lineChunks = 0;
+
+// create an array to store the number of vertexes in a chunk
+let vertexesInChunk = new Array();
+
+// create an index for the array to know which chunk we are in.
+let vertexChunk = 0;
+
+for await(const line of readLine)
+{
+   if(line.startsWith("*"))
+      hitStars = true;
+
+   if(!hitStars)  // write all lines up to the star line
+      outputStream.write(line + "\n");
+   else if(hitStars && line.startsWith("*"))// write the star line
+      outputStream.write(line + "\n");
+   else if(hitStars)//we are now past the star line so we have numerical data to use
+   {
+      linesCounted += 1;
+
+      if(linesCounted == 1)// handle the first lin: top bottom right left
+      {
+         const nums = line.split("  ");
+         
+         const left  = +nums[0];
+         const top   = +nums[1];
+         const right = +nums[2];
+         const bot   = +nums[3];
+
+         outputStream.write((left  + xTrans).toFixed(6) + "  " +
+                            (top   + yTrans).toFixed(6) + "  " + 
+                            (right + xTrans).toFixed(6) + "  " + 
+                            (bot   + yTrans).toFixed(6) + "  " + "\n");
+      }
+      else if(linesCounted == 2)// handle the second line, the number of line chunks
+      {
+         lineChunks = +line;
+         outputStream.write(line + "\n");
+      }
+      else if(linesCounted == 3)// handle the third line, the number of vertices in this chunk
+      {
+         vertexesInChunk[vertexChunk++] = +line;
+         outputStream.write(line + "\n");
+      }
+      else if(line.startsWith("  "))// next is the vertexes in this chunk which start with '  '
+      {
+         const nums = line.split("  ");
+
+         const x = (+nums[1]) + xTrans;
+         const y = (+nums[2]) + yTrans;
+
+         outputStream.write("  " + x.toFixed(4) + 
+                            "  " + y.toFixed(4) + "\n");
+      }
+      else if(!isNaN(line))// make sure line is numerical since all that should be left is the next vertex count 
+      {
+         vertexesInChunk[vertexChunk++] = +line;
+         outputStream.write(line + "\n");
+      }
+      else // unknown data so record it to console
+      {
+         outputStream.write(line + "\n");
+         console.log(line);
+      }
+
+   }
+}
+
+/*
+// create a variable to keep track of if we have hit the star line
+let hitStars = false;
+
+// create a variable to keep track of if we have hit the vertex data
+let hitVertexes = false;
+
+// keep track of how many lines we have read to allow us to handle the first three lines after * line
+let numLinesCounted = 0;
+
+// the number of line strips to be found in the 2nd line of text and used in the for loops for vertexes
+let numLineStrips = 0; 
+
+// the number of vertices to be found in the 3rd line of text and used in the for loops for vertexes
+let numVertices = 0; 
+for await (const line of readLine)
+{
+   // record if we have hit the star line
+   if(line.startsWith("*"))
+      hitStars = true; 
+
+   // if we haven't hit the star line then just keep writing the data to the file
+   if(!hitStars)
+      outputStream.write(line + "\n");
+   else // if we have hit the star line then we need to treat each line individually
+   {
+      // if we have the star line record it 
+      if(line.startsWith("*"))
+         outputStream.write(line + "\n"); 
+      else  // we are reading a line past the star line so these are data that need to be transformed
+      {
+         // record we are reading a line past the * line
+         numLinesCounted += 1;
+
+         // record whether we have hit vertexes or are still getting the start info
+         if(line.startsWith("  "))
+            hitVertexes = true;
+      
+         // the first line is the start data, bottom left top and right
+         if(!hitVertexes && numLinesCounted == 1)
+         {
+            const nums = line.split("  ");
+            
+            const left  = +nums[0];
+            const top   = +nums[1];
+            const right = +nums[2];
+            const bot   = +nums[3];
+            
+            outputStream.write((left  + xTrans).toFixed(6) + "  " +
+                               (top   + yTrans).toFixed(6) + "  " + 
+                               (right + xTrans).toFixed(6) + "  " + 
+                               (bot   + yTrans).toFixed(6) + "  " + "\n");
+         }// end if for the first line
+         // the second line is the number of vertexes
+         else if(!hitVertexes && numLinesCounted == 2)
+         {
+            numLineStrips = +line;
+            outputStream.write(numLineStrips + "\n");
+         }// end if for the second line
+         else if(!hitVertexes && numLinesCounted == 3)
+         {
+            numVertices = +line;
+            outputStream.write(numVertices + "\n");
+         }
+         else if(!hitVertexes && numLinesCounted > 3)
+         {
+            if(!line.startsWith("  "))
+               break();
+            else
+
+            for(let j = 0; j < numLineStrips; j++)
+            {
+               // copy the number of vertices in this line-strip
+               const numVertices = +line;
+               outputStream.write(numVertices + "\n");
+            
+               for(let i = 0; i < numVertices; i++)
+               {
+                  const nums = line.split("  ");
+               
+                  const x = (+nums[1]) + xTrans;
+                  const y = (+nums[2]) + yTrans;
+               
+                  outputStream.write("  " + x.toFixed(4) + 
+                                     "  " + y.toFixed(4) + "\n");
+               }// end for
+            }// end for
+         }// end if for the vertexes
+      }// end else for past the star line, recorded star line, and dealing with data 
+   }// end else for past star line but not yet recorded star line
+}// end for loop to read lines
+
+inputStream.close();
+outputStream.close();
+*/
+
+/*
+import {createReadStream, createWriteStream} from "node:fs";
+import * as ReadLine from "node:readline";
+
+if(process.argv.length != 5)
+   throw new Error("Usage: node TranslateGRS.js <GRS-File-Name> [<x-amount> [<y-amount>]]");
+
+const file = process.argv[2];
+const xTrans = +process.argv[3];
+const yTrans = +process.argv[4];
+
+const inputStream = createReadStream(file);
+const readLine = ReadLine.createInterface(
+                           { input: inputStream, 
+                             crlfDelay: Infinity
+                           });
+
+const outputPath = file.substring(1 + file.indexOf("\\"), file.indexOf(".")) 
+                                    + "-Trans(" + xTrans + ", " + yTrans + ").grs";
+const outputStream = createWriteStream(outputPath, { encoding: "utf8" });
+
+// this doesn't work because we need to progress to the next line
+// while handling the data becase each line gets treated seperately
+// can't treat each line the same
+
+
+// create a variable to keep track of if we have hit the star line
+let hitStars = false;
+
+for await (const line of readLine)
+{
+   // record if we have hit the star line
+   if(line.startsWith("*"))
+      hitStars = true; 
+
+   // if we haven't hit the star line then just keep writing the data to the file
+   if(!hitStars)
+      outputStream.write(line + "\n");
+   else // if we have hit the star line then we need to treat each line individually
+   {
+      // if we have the star line record it 
+      if(line.startsWith("*"))
+         outputStream.write(line + "\n"); 
+      else  // we are reading a line past the star line so these are data that need to be transformed
+      {
+         const nums = line.split("  ");
+         
+         const left  = +nums[0];
+         const top   = +nums[1];
+         const right = +nums[2];
+         const bot   = +nums[3];
+         
+         outputStream.write((left  + xTrans).toFixed(6) + "  " +
+                            (top   + yTrans).toFixed(6) + "  " + 
+                            (right + xTrans).toFixed(6) + "  " + 
+                            (bot   + yTrans).toFixed(6) + "  " + "\n");
+         
+         const numLineStrips = +line;
+         outputStream.write(numLineStrips + "\n");
+         
+         for(let j = 0; j < numLineStrips; j++)
+         {
+            // copy the number of vertices in this line-strip
+            const numVertices = +line;
+            outputStream.write(numVertices + "\n");
+         
+            for(let i = 0; i < numVertices; i++)
+            {
+               const nums = line.split("  ");
+            
+               const x = (+nums[1]) + xTrans;
+               const y = (+nums[2]) + yTrans;
+            
+               outputStream.write("  " + x.toFixed(4) + 
+                                  "  " + y.toFixed(4) + "\n");
+            }
+         }
+      }
+   }
+}
+inputStream.close();
+outputStream.close();
+*/
+
+/*
+import {createReadStream, createWriteStream} from "node:fs";
+
+if(process.argv.length != 5)
+   throw new Error("Usage: node TranslateGRS.js <GRS-File-Name> [<x-amount> [<y-amount>]]");
+
+const file = process.argv[2];
+const xTrans = +process.argv[3];
+const yTrans = +process.argv[4];
+
+const inputStream = createReadStream(file);
+inputStream.setEncoding("utf-8");
+
+const outputPath = file.substring(1 + file.indexOf("\\"), file.indexOf(".")) 
+                                    + "-Trans(" + xTrans + ", " + yTrans + ").grs";
+const outputStream = createWriteStream(outputPath);
+outputStream.setDefaultEncoding("utf-8");
+
+let incompleteLine = " ";
+
+console.log("about to enter function");
+inputStream.on("data", 
+            (chunk) => 
+            {
+
+console.log("Entered fucntion");
+
+               let lines = (incompleteLine + chunk).split("\n");
+               incompleteLine = lines.pop();
+
+console.log(lines);
+
+               for(let line of lines)
+               {
+                  while(!line.startsWith("*"))
+                     outputStream.write(line);
+
+                  const nums = line.split("  ");
+                  const left  = +nums[0];
+                  const top   = +nums[1];
+                  const right = +nums[2];
+                  const bot   = +nums[3];
+
+                  outputStream.write((left  + xTrans).toFixed(6) + "  " +
+                                     (top   + yTrans).toFixed(6) + "  " + 
+                                     (right + xTrans).toFixed(6) + "  " + 
+                                     (bot   + yTrans).toFixed(6) + "  " + "\n");
+
+                  const numLineStrips = +line;
+                  outputStream.write(numLineStrips + "\n");
+
+                  for(let j = 0; j < numLineStrips; j++)
+                  {
+                     // copy the number of vertices in this line-strip
+                     const numVertices = +line;
+                     outputStream.write(numVertices + "\n");
+                     for(let i = 0; i < numVertices; i++)
+                     {
+                        const nums = line.split("  ");
+                        const x = (+nums[1]) + xTrans;
+                        const y = (+nums[2]) + yTrans;
+                        outputStream.write("  " + x.toFixed(4) + 
+                                           "  " + y.toFixed(4) + "\n");
+                     }
+                  }
+               }
+            });
+inputStream.close();
+outputStream.close();
+*/
+
+/*
+import {createReadStream, createWriteStream} from "node:fs";
+import * as ReadLine from "node:readline";
+
+if(process.argv.length != 5)
+   throw new Error("Usage: node TranslateGRS.js <GRS-File-Name> [<x-amount> [<y-amount>]]");
+
+const file = process.argv[2];
+const xTrans = +process.argv[3];
+const yTrans = +process.argv[4];
+
+const inputStream = createReadStream(file);
+const readLine = ReadLine.createInterface(
+                           { input: inputStream, 
+                             crlfDelay: Infinity
+                           });
+
+const outputPath = file.substring(1 + file.indexOf("\\"), file.indexOf(".")) 
+                                    + "-Trans(" + xTrans + ", " + yTrans + ").grs";
+const outputStream = createWriteStream(outputPath, { encoding: "utf8" });
+
+// this doesn't work because we need to progress to the next line
+// but we can't get next line until we exit the while loop
+// but we can't exit the while loop until we get the next line
+
+for await (const line of readLine)
+{
+// this doesn't work because we need to progress to the next line
+// but we can't get next line until we exit the while loop
+// but we can't exit the while loop until we get the next line
+
+   while(!line.startsWith("*"))
+      outputStream.write(line);
+
+   const nums = line.split("  ");
+
+   const left  = +nums[0];
+   const top   = +nums[1];
+   const right = +nums[2];
+   const bot   = +nums[3];
+
+   outputStream.write((left  + xTrans).toFixed(6) + "  " +
+                      (top   + yTrans).toFixed(6) + "  " + 
+                      (right + xTrans).toFixed(6) + "  " + 
+                      (bot   + yTrans).toFixed(6) + "  " + "\n");
+
+   const numLineStrips = +line;
+   outputStream.write(numLineStrips + "\n");
+
+   for(let j = 0; j < numLineStrips; j++)
+   {
+      // copy the number of vertices in this line-strip
+      const numVertices = +line;
+      outputStream.write(numVertices + "\n");
+
+      for(let i = 0; i < numVertices; i++)
+      {
+         const nums = line.split("  ");
+
+         const x = (+nums[1]) + xTrans;
+         const y = (+nums[2]) + yTrans;
+
+         outputStream.write("  " + x.toFixed(4) + 
+                            "  " + y.toFixed(4) + "\n");
+      }
+   }
+}
+inputStream.close();
+outputStream.close();
+*/
+
+/*
 import {createWriteStream, readFileSync} from "node:fs";
 
 if(process.argv.length != 5)
@@ -64,7 +483,7 @@ for(let j = 0; j < numLineStrips; j++)
 }
 outputStream.close();
 
-
+*/
 
 
 //const inputPath  = file;
