884170bef835814ce9c00cc45100d6eb2d8edca7
Chris Wayman <wayman0@pnw.edu>
2023-11-01 19:02:56 -0500

    Added resizer to the html document for rendering

--- code_editor.js
+++ code_editor.js
@@ -1,9 +1,31 @@
 //@ts-check
 
+// get and set the canvas to be black to start with
+const resizer = document.getElementById("resizer");
+const w = resizer?.offsetWidth;
+const h = resizer?.offsetHeight;
+        
+//@ts-ignore
+const ctx = document.getElementById("pixels").getContext("2d");
+// @ts-ignore
+if (ctx == null)
+{
+   console.log("cn.getContext(2d) is null");
+}
+
+ctx.canvas.width = w;
+ctx.canvas.height = h;
+
+//@ts-ignore
+const black = new Uint8ClampedArray(w * h * 4);
+for(let x = 3; x < black.length; x += 4)
+    black[x] = 255;
+
+//@ts-ignore
+ctx.putImageData(new ImageData(black, w, h), 0, 0);
+
 // access the button on the html file
 const runButton = document.getElementById("run");
-
-//console.log(runButton);
 
 // add the runCode function to the button's event listener
 runButton?.addEventListener("click", runCode);
