9bb3b9e44861234180fc18cc1924ad4177949c9b
Chris Wayman <wayman0@pnw.edu>
2024-01-22 20:57:55 -0600

    started work on pipeline timing

---
+++ PipelineImplementationTiming/Pipeline-loop/Project.js
@@ -0,0 +1,109 @@
+/*
+ * Renderer 10. The MIT License.
+ * Copyright (c) 2022 rlkraft@pnw.edu
+ * See LICENSE for details.
+*/
+
+/**
+   Project each {@link Vertex} of a {@link Model} from camera
+   coordinates to the {@link Camera}'s image plane {@code z = -1}.
+<p>
+   Let us derive the formulas for the perspective projection
+   transformation (the formulas for the parallel projection
+   transformation are pretty obvious). We will derive the
+   x-coordinate formula; the y-coordinate formula is similar.
+<p>
+   Let {@code (x_c, y_c, z_c)} denote a point in the 3-dimensional
+   camera coordinate system. Let {@code (x_p, y_p, -1)} denote the
+   point's perspective projection into the image plane, {@code z = -1}.
+   Here is a "picture" of just the xz-plane from camera space. This
+   picture shows the point {@code (x_c, z_c)} and its projection to
+   the point {@code (x_p, -1)} in the image plane.
+<pre>{@code
+           x
+           |                             /
+           |                           /
+       x_c +                         + (x_c, z_c)
+           |                       / |
+           |                     /   |
+           |                   /     |
+           |                 /       |
+           |               /         |
+           |             /           |
+       x_p +           +             |
+           |         / |             |
+           |       /   |             |
+           |     /     |             |
+           |   /       |             |
+           | /         |             |
+           +-----------+-------------+------------> -z
+        (0,0)         -1            z_c
+}</pre>
+<p>
+   We are looking for a formula that computes {@code x_p} in terms of
+   {@code x_c} and {@code z_c}. There are two similar triangles in this
+   picture that share a vertex at the origin. Using the properties of
+   similar triangles we have the following ratios. (Remember that these
+   are ratios of positive lengths, so we write {@code -z_c}, since
+   {@code z_c} is on the negative z-axis).
+<pre>{@code
+                 x_p       x_c
+                -----  =  -----
+                  1       -z_c
+}</pre>
+<p>
+   If we solve this ratio for the unknown, {@code x_p}, we get the
+   projection formula,
+<pre>{@code
+                 x_p = -x_c / z_c.
+}</pre>
+<p>
+   The equivalent formula for the y-coordinate is
+<pre>{@code
+                 y_p = -y_c / z_c.
+}</pre>
+*/
+
+//@ts-check
+import {Model, Vertex, Camera} from "../../renderer/scene/SceneExport.js";
+
+/**
+ *  Project each {@link Vertex} from a {@link Model} to
+    the {@link Camera}'s image plane {@code z = -1}.
+
+ * @param {Model} model the model whose vertexes are to be projected
+ * @param {Camera} camera the scenes camera object
+ * @returns {Model} a new model containing the projected vertexes
+ */
+export default function project(model, camera)
+{
+   
+    const newVertexList = new Array();
+    for (let x = 0; x < model.vertexList.length; x += 1)
+    {
+        const v = model.vertexList[x];
+
+        if (camera.perspective)
+            newVertexList[x] = new Vertex(v.x/-v.z, v.y/-v.z, -1);
+        else
+            newVertexList[x] = new Vertex(v.x, v.y, 0);
+    }
+    
+
+    
+    // this method is slower
+    //const newVertexList = model.vertexList.map(
+    //                      (v) => {
+    //                                if(camera.perspective)
+    //                                   return new Vertex(v.x/-v.z, v.y/-v.z, -1);
+    //                                else
+    //                                   return new Vertex(v.x, v.y, 0);
+    //                             });
+    
+
+    return new Model(newVertexList,
+                     model.primitiveList,
+                     model.colorList,
+                     model.name,
+                     model.visible);
+}
---
+++ PipelineImplementationTiming/Pipeline-loop/test.js
@@ -0,0 +1,43 @@
+//Import what is necessary for your code
+import {Scene, Position, Matrix, Model, Vertex, LineSegment} from "../../renderer/scene/SceneExport.js";
+import {renderFB, setDoAntiAliasing, doAntiAliasing, setDoGamma, doGamma} from "../../renderer/pipeline/PipelineExport.js";
+import {FrameBuffer, Viewport, Color} from "../../renderer/framebuffer/FramebufferExport.js";
+import {Sphere} from "../../renderer/models_L/ModelsExport.js";
+import * as ModelShading from "../../renderer/scene/util/UtilExport.js";
+
+console.log("Sphere Rotate 1 Degree for 360 Degrees");
+
+//Create a default sphere from the Models import
+const sphereMod = new Sphere();
+//Set the sphere to be a random color using the Model Shading import
+ModelShading.setRandomColor(sphereMod);
+
+//Create a position to hold the model
+const spherePos = Position.buildFromModel(sphereMod);
+//Set the position to be translated back 3 units
+spherePos.setMatrix(Matrix.translate(0, 0, -3));
+
+//Create an empy scene
+const scene = new Scene();
+//Add the sphere position to the scene
+scene.addPosition(spherePos);
+
+let time = 0;
+const fb = new FrameBuffer(600, 600, Color.black);
+for(let rot = 0; rot < 360; rot += 1)
+{
+    spherePos.getMatrix().mult(Matrix.rotateY(1));
+
+    const startTime = new Date().getTime();
+    renderFB(scene, fb);
+    const endTime = new Date().getTime();
+
+    console.log(endTime - startTime);
+    time += endTime - startTime;
+}
+
+console.log(time/360);
+
+
+
+
