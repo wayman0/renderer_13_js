0c8a72c1408f9a803d1aaafe9e20d9e3eca4342f
Chris Wayman <wayman0@pnw.edu>
2024-04-27 00:43:31 -0500

    Added Triangluars

---
+++ renderer/models_LP/TriangularPrism.js
@@ -0,0 +1,181 @@
+/*
+ * Renderer Models. The MIT License.
+ * Copyright (c) 2022 rlkraft@pnw.edu
+ * See LICENSE for details.
+*/
+
+/**
+   Create a wireframe model of a right equilateral triangular prism
+   with the y-axis as its central axis.
+<p>
+   See <a href="https://en.wikipedia.org/wiki/Triangular_prism" target="_top">
+                https://en.wikipedia.org/wiki/Triangular_prism</a>
+<p>
+   See <a href="https://en.wikipedia.org/wiki/Prism_(geometry)" target="_top">
+                https://en.wikipedia.org/wiki/Prism_(geometry)</a>
+<p>
+   Attach to each triangular end of the prism a tetrahedron.
+*/
+//@ts-check 
+
+import {Model, Vertex, LineSegment, LineFan, LineLoop, Lines} from "../scene/SceneExport.js";
+import {format} from "../scene/util/UtilExport.js";
+
+export default class TriangularPrism extends Model
+{
+//   /**
+//      Create a right equilateral triangular prism with a
+//      regular tetrahedrons attached to each end so that
+//      the total length runs from -1 to 1 along the y-axis.
+//   */
+//   public TriangularPrism( )
+//   {
+//      this(0.5, 0.6);
+//   }
+//
+//
+//   /**
+//      Create an equilateral triangular prism that runs
+//      from {@code -h} to {@code h} along the y-axis, has
+//      triangle side length {@code s}, and has a regular
+//      tetrahedron attached to each end.
+//
+//      @param s  the length of the triangle's sides
+//      @param h  the body of the prism runs from -h to h along the y-axis
+//   */
+//   public TriangularPrism(final double s, final double h)
+//   {
+//      this(s, h, 0);
+//   }
+//
+//
+//   /**
+//      Create an equilateral triangular prism that runs
+//      from {@code -h} to {@code h} along the y-axis, has
+//      triangle side length {@code s}, has a regular
+//      tetrahedron attached to each end, and has {@code n}
+//      lines of latitude around the body of the prism.
+//
+//      @param s  the length of the triangle's sides
+//      @param h  the body of the prism runs from -h to h along the y-axis
+//      @param n  number of lines of latitude around the body of the prism
+//      @throws IllegalArgumentException if {@code n} is less than 0
+//   */
+//   public TriangularPrism(final double s, final double h,
+//                          final int n)
+//   {
+//      this(s/Math.sqrt(3), h, Math.atan(Math.sqrt(2)), n);
+//   }
+//
+//
+//   /**
+//      Create an equilateral triangular prism that runs
+//      from {@code -h} to {@code h} along the y-axis, with
+//      the triangle inscribed in a circle of radius {@code r},
+//      has a tetrahedron attached to each end where the
+//      face-edge-face angle of each tetrahedron is {@code theta}
+//      (with theta in radians!), and has {@code n} lines of
+//      latitude around the body of the prism.
+//   <p>
+//      If {@code theta = 0}, then there are no tetrahedrons at the ends of the prism.
+//   <p>
+//      If {@code theta = arctan(sqrt(2)) = 54.736�}, then the tetrahedrons are regular.
+//
+//      @param r      radius of circle in xz-plane that the equilateral triangle is inscribed in
+//      @param h      the body of the prism runs from -h to h along the y-axis
+//      @param theta  slant angle of each tetrahedron at the ends of the prism
+//      @param n      number of lines of latitude around the body of the prism
+//      @throws IllegalArgumentException if {@code n} is less than 0
+//   */
+//   public TriangularPrism(final double r, final double h, final double theta,
+//                          final int n)
+//   {
+//      this(r, h, r*Math.tan(theta), n, true);
+//   }
+//
+   /**
+      Create an equilateral triangular prism that runs
+      from {@code -h} to {@code h} along the y-axis, with
+      the triangle inscribed in a circle of radius {@code r},
+      has a tetrahedron attached to each end where the height
+      of each tetrahedron is {@code h2}, and has {@code n} lines
+      of latitude around the body of the prism.
+   <p>
+      So the total height is {@code 2*(h + h2)}.
+      @param {number} [r=.5/Math.sqrt(3)]  radius of circle in xz-plane that the equilateral triangle is insc
+      @param {number} [h=.6]  the body of the prism runs from h to -h in the y-direction
+      @param {number} [h2=r*Math.tan(Math.atan(Math.sqrt(2)))]  height of each tetrahedron at the ends of the
+      @param {number} [n=3]  number of lines of latitude around the body of the prism
+      @param {boolean} [bothHalves=true]  determines if both halves or only the top half gets created
+   */
+   constructor(r=.5, h=.6, h2=r*Math.tan(Math.atan(Math.sqrt(2))), n=3, bothHalves=true)
+   {
+      super(undefined, undefined, undefined, undefined, undefined,  format("Triangular Prism(%.2f,%.2f,%.2f,%d)", r, h, h, n));
+      
+      if(n < 0)
+         throw new Error("n must be greater than or equal to 0");
+
+      // Create the prism's geometry.
+      const sqrt3 = Math.sqrt(3.0);
+      let v0, v1, v2, v3, v4, v5, v6, v7;
+
+      // Three vertices around the top.
+      v0 = new Vertex( r,    h,    0);
+      v1 = new Vertex(-r/2,  h,  r*0.5*sqrt3);
+      v2 = new Vertex(-r/2,  h, -r*0.5*sqrt3);
+
+      // Three vertices around the bottom.
+      if (bothHalves)
+      {
+         v3 = new Vertex( r,   -h,    0);
+         v4 = new Vertex(-r/2, -h,  r*0.5*sqrt3);
+         v5 = new Vertex(-r/2, -h, -r*0.5*sqrt3);
+      }
+      else // ! bothHalves, so cut off the bottom half
+      {
+         v3 = new Vertex( r,    0,    0);
+         v4 = new Vertex(-r/2,  0,  r*0.5*sqrt3);
+         v5 = new Vertex(-r/2,  0, -r*0.5*sqrt3);
+      }
+      v6 = new Vertex(0,  h+h2, 0);  // vertex at the top
+      if (bothHalves)
+         v7 = new Vertex(0, -h-h2, 0);  // vertex at the bottom
+      else // ! bothHalves, so cut off the bottom half
+         v7 = new Vertex(0, 0, 0);   // vertex at the bottom
+      this.addVertex(v0, v1, v2, v3, v4, v5, v6, v7);
+      let index = 8;
+      // Create 15 line segments.
+      // 3 top faces
+      this.addPrimitive(new LineFan(6, 0, 1, 2));
+      // the top edge
+      this.addPrimitive(new LineLoop(0, 1, 2));
+                                                              // three vertical edges
+      this.addPrimitive(new Lines(0,3, 1,4, 2,5));
+
+      // the bottom edge
+      this.addPrimitive(new LineLoop(3, 4, 5));
+
+      // 3 bottom faces
+      this.addPrimitive(new LineFan(7, 3, 4, 5));
+
+      // Create n lines of latitude around the prism.
+      if (n > 0)
+      {
+         let delta_y = 2.0*h/(n+1);
+         if (! bothHalves)  // cut off the bottom half
+            delta_y = h/(n+1);
+         for(let j = 0; j < n; ++j)
+         {
+            let y = -h + (j+1) * delta_y;
+            if (! bothHalves)  // cut off the bottom half
+               y = (j+1) * delta_y;
+            this.addVertex(new Vertex( r,    y,    0),
+                           new Vertex(-r/2,  y,  r*0.5*sqrt3),
+                           new Vertex(-r/2,  y, -r*0.5*sqrt3));
+            
+            this.addPrimitive(new LineLoop(index+0, index+1, index+2));
+            index += 3;
+         }
+      }
+   }
+}//TriangularPrism
---
+++ renderer/models_LP/TriangularPyramid.js
@@ -0,0 +1,159 @@
+/*
+ * Renderer Models. The MIT License.
+ * Copyright (c) 2022 rlkraft@pnw.edu
+ * See LICENSE for details.
+*/
+
+/**
+   Create a wireframe model of a tetrahedron as a
+   triangular pyramid with an equilateral triangle
+   base (centered at the origin in the xz-plane)
+   whose three vertices are connected to a 4th vertex
+   on the positive y-axis.
+
+   @see Tetrahedron
+*/
+//@ts-check
+import {Model, Vertex, LineSegment, LineFan, LineLoop} from "../scene/SceneExport.js";
+import {format} from "../scene/util/UtilExport.js";
+
+export default class TriangularPyramid extends Model
+{
+   /**@type {number} */ #r;
+   /**@type {number} */ #h;
+   /**@type {number} */ #n;
+   /**@type {number} */ #k;
+
+   /**
+      Create a tetrahedron with one face being an equilateral triangle
+      inscribed in a circle of radius {@code r} centered at the origin
+      of the xz-plane and with the 4th vertex on the y-axis at height
+      {@code h}.
+   <p>
+      If {@code h = r * sqrt(2)}, then the tetrahedron is a regular tetrahedron.
+      with side length {@code s = r * sqrt(3)}.
+   <p>
+      Another way to state this is, if an equilateral triangle is inscribed
+      in a circle of radius {@code r}, then the edge length of the triangle
+      is {@code r*sqrt(3)} and the height of the regular tetrahedron made
+      from the triangle is {@code r*sqrt(2)}.
+
+      @param {number} [r=(Math.sqrt(3)/Math.sqrt(2))/Math.sqrt(3)]  radius of circle in xz-plane that the equilateral base is inscribed in
+      @param {number} [h=(Math.sqrt(3)/Math.sqrt(2))*(Math.sqrt(2)/Math.sqrt(3))]  coordinate on the y-axis of the apex
+      @param {number} [n=1]  number of lines of latitude around the body of the pyramid
+      @param {number} [k=1]  number of triangles in the triangle fan at the top of each side
+   */
+   constructor(r=(Math.sqrt(3)/Math.sqrt(2))/Math.sqrt(3), h=(Math.sqrt(3)/Math.sqrt(2))*(Math.sqrt(2)/Math.sqrt(3)), n=1, k=1)
+   {
+      super(undefined, undefined, undefined, undefined, undefined,  format("Triangular_Pyramid(%.2f,%.2f,%d,%d)", r,   h,   n, k));
+
+      if (n < 1)
+         throw new Error("n must be greater than 0");
+      if (k < 1)
+         throw new Error("k must be greater than 0");
+
+      this.#r = r;
+      this.#h = h;
+      this.#n = n;
+      this.#k = k;
+
+      // Create the pyramid's geometry.
+      const apex = new Vertex(0, h, 0),
+            centerVertex = new Vertex(0, 0, 0);
+      
+      this.addVertex(apex, centerVertex);
+
+      const apexIndex = 0;
+      const centerIndex = 1;
+      let index = 2;
+
+      // Create all the lines of "longitude" from the apex, down
+      // to the base, and then to the center of the base.
+      const sqrt3 = Math.sqrt(3.0);
+      // Three vertices around the bottom face.
+      const v0 = new Vertex( r,   0,    0);
+      const v1 = new Vertex(-r/2, 0,  r*0.5*sqrt3);
+      const v2 = new Vertex(-r/2, 0, -r*0.5*sqrt3);
+      
+      const botFan = new LineFan();
+      const topFan = new LineFan();
+
+      botFan.addIndex(centerIndex);
+      topFan.addIndex(apexIndex);
+
+      for(let j = 0; j < k; ++j)
+      {
+         const t = j * (1.0 / k);
+         // use linear interpolation (lerp)
+         this.addVertex( new Vertex(
+         //         (1-t)*v0  +  t*v1
+                    (1-t)*v0.x + t*v1.x,
+                    (1-t)*v0.y + t*v1.y,
+                    (1-t)*v0.z + t*v1.z ));
+         this.addVertex( new Vertex(
+         //         (1-t)*v1  +  t*v2
+                    (1-t)*v1.x + t*v2.x,
+                    (1-t)*v1.y + t*v2.y,
+                    (1-t)*v1.z + t*v2.z ));
+         this.addVertex( new Vertex(
+         //         (1-t)*v2  +  t*v0
+                    (1-t)*v2.x + t*v0.x,
+                    (1-t)*v2.y + t*v0.y,
+                    (1-t)*v2.z + t*v0.z ));
+
+         botFan.addIndex(index+0, index+1, index+2);
+         topFan.addIndex(index+0, index+1, index+2);
+         index += 3;
+      }
+
+      this.addPrimitive(botFan, topFan);
+
+      // Create all the lines of "latitude" around the pyramid, starting
+      // from the base and working upwards.
+      for(let i = 0; i < n; ++i)
+      {
+         const t = i * (1.0 / n);
+         // Use linear interpolation (lerp).
+         this.addVertex( new Vertex(
+         //         (1-t)*v0   + t*apex
+                    (1-t)*v0.x + t*apex.x,
+                    (1-t)*v0.y + t*apex.y,
+                    (1-t)*v0.z + t*apex.z ));
+         this.addVertex( new Vertex(
+         //         (1-t)*v1   + t*apex
+                    (1-t)*v1.x + t*apex.x,
+                    (1-t)*v1.y + t*apex.y,
+                    (1-t)*v1.z + t*apex.z ));
+         this.addVertex( new Vertex(
+         //         (1-t)*v2   + t*apex
+                    (1-t)*v2.x + t*apex.x,
+                    (1-t)*v2.y + t*apex.y,
+                    (1-t)*v2.z + t*apex.z ));
+
+         this.addPrimitive(new LineLoop(index+0, index+1, index+2));
+
+         index += 3;
+      }
+   }
+   
+   getHorizCount()
+   {
+      return this.#n;
+   }
+
+   getVertCount()
+   {
+      return this.#k;
+   }
+
+   /**
+    * Build a new Model using the same parameters but the given line counts
+    * @param {number} n the new horizontal line count
+    * @param {number} k the new vertical line count
+    * @returns {TriangularPyramid} the new model with the same parameters but different line counts
+    */
+   remake(n, k)
+   {
+      return new TriangularPyramid(this.#r, this.#h, n, k);
+   }
+}//TriangularPyramid
