e962e2ff7aa2940b9ca99327c4643745b1fe7022
Chris Wayman <wayman0@pnw.edu>
2024-04-27 00:55:52 -0500

    Everything should be up to date with linking to 13

--- viewWebsite.cmd
+++ viewWebsite.cmd
@@ -1,2 +1,2 @@
 IF "%1" == "" (start msedge wayman0.github.io/renderer_13_js) ELSE (start msedge wayman0.github.io/renderer_13_js/%1)
-ECHO start msedge wayman0.github.io/renderer_12_js
+ECHO start msedge wayman0.github.io/renderer_13_js
