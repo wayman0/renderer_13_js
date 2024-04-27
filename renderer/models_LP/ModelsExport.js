87302022ac2336f5a9ada72d3e20d7251adb37cb
Chris Wayman <wayman0@pnw.edu>
2024-04-26 21:30:32 -0500

    Added Cubes

--- renderer/models_LP/ModelsExport.js
+++ renderer/models_LP/ModelsExport.js
@@ -7,3 +7,7 @@ export {default as CircleSector} from "./CircleSector.js";
 export {default as Cone} from "./Cone.js";
 export {default as ConeSector} from "./ConeSector.js";
 export {default as ConeFrustum} from "./ConeFrustum.js";
+export {default as Cube}  from "./Cube.js";
+export {default as Cube2} from "./Cube2.js";
+export {default as Cube3} from "./Cube3.js";
+export {default as Cube4} from "./Cube4.js";
