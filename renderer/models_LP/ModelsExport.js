b934702d4300e1b0ac408c774268b9a43f19a5ff
Chris Wayman <wayman0@pnw.edu>
2024-04-26 23:58:03 -0500

    added ico and icodo

--- renderer/models_LP/ModelsExport.js
+++ renderer/models_LP/ModelsExport.js
@@ -17,4 +17,6 @@ export {default as Disk} from "./Disk.js";
 export {default as DiskSector} from "./DiskSector.js";
 export {default as Dodecahedron} from "./Dodecahedron.js";
 export {default as Icosahedron} from "./Icosahedron.js";
-export {default as icosidodecahedron} from "./Icosidodecahedron.js";
+export {default as Icosidodecahedron} from "./Icosidodecahedron.js";
+export {default as Octahedron} from "./Octahedron.js";
+export {default as Tetrahedron} from "./Tetrahedron.js";
